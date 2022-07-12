fed631f42a07c7070cc2defe3069c00740d6a96f
Gsaila <105499852+Gsaila@users.noreply.github.com>
2022-07-12 11:31:21 -0300

    VPC creation

--- variable.tf
+++ variable.tf
@@ -6,14 +6,14 @@ variable "vpc_cidr" {
 }
 variable "subnets_cidr" {
     type = list
-    default = ["10.20.0.0/16", "10.20.3.0/24"]
+    default = ["10.20.1.0/24", "10.20.2.0/24"]
 }
 variable "azs" {
     type = list
     default = ["us-east-1a", "us-east-1b"]
 }
 variable "amiid" {
-    default = "ami-08d4ac5b634553e16"
+    default = "ami-0cff7528ff583bf9a"
 }
 variable "instance_type" {
     default = "t2.micro"
